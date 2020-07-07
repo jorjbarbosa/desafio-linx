26ba85f77b217abf25a12f83c93455e0e04e5a4f
Jorge Barbosa <jorge@flexpeak.com.br>
2020-07-07 19:50:41 -0400

    Adicionando rota de produtos

--- api-catalogo/src/server.js
+++ api-catalogo/src/server.js
@@ -1,5 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.use('/products', require('./routes/products'))
 
 app.listen(3000, () => {
   console.log('Server running on port 3000')
