1d692392d9d77db05e669027144cae88ee93abab
Jorge Barbosa <jorge@flexpeak.com.br>
2020-07-12 21:31:44 -0400

    Adicionando Label no card de produtos

--- frontend/assets/js/app.js
+++ frontend/assets/js/app.js
@@ -14,14 +14,15 @@ const getProducts = () => {
 
 const renderProducts = (data, type) => {
   const carousel = document.querySelector(`#${type} .carousel-slide`)
-  data.map((product) => {
+  data.map((product, index) => {
     const item = `<div class="products">
                     <div class="product">
                       <img src="http://${product.images.default}" class="product-image" alt="">
                       <h4 class="product-name">${truncateString(product.name, 70)}</h4>
-                      <p class="product-old-price">R$ ${product.oldPrice}</p>
-                      <p>Por <span class="price-span">${product.price}</span></p>
-                      <p>${product.installment.count}x R$ ${product.installment.price}</p>
+                      <p class="product-old-price">${formatarValor(product.oldPrice)}</p>
+                      <p>Por <span class="price-span">${formatarValor(product.price)}</span></p>
+                      <p>${product.installment.count}x ${formatarValor(product.installment.price)}</p>
+                      <span class="${type == 'mostpopular' ? 'mostpopular-label' : 'pricereduction-label'}">${type === 'mostpopular' ? (index + 1) + 'º'  : getDiscount(product.oldPrice, product.price)}</span>
                     </div>
                   </div>`
     carousel.innerHTML += item
@@ -35,4 +36,13 @@ const truncateString = (str, num) => {
     return str
   
     return str.slice(0, num) + '...'
+}
+
+const formatarValor = (valor) => {
+  return valor.toString().toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+}
+
+const getDiscount = (oldValue, newValue) => {
+  const discount = (((newValue - oldValue)/oldValue)*100).toFixed(0)
+  return `${discount}%`
 }
